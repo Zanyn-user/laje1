bb744635f73ec465c396ebe8dc54b747fbd8a1c3
Zanyn-user <zanynuser@gmail.com>
2024-10-30 16:07:10 -0500

    cualquier cosa

---
+++ src/main/java/com/mycompany/laje1/Laje1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laje1;
+
+/**
+ *
+ * @author User
+ */
+public class Laje1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
